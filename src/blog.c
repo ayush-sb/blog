dcb06ef08897ec3fd52b9ae4c4fe9e413fb0f165
Ayush Singh Bhandari <ayush.sb2003@gmail.com>
2024-02-07 23:54:12 +0530

    Fix bad fd error

--- src/blog.c
+++ src/blog.c
@@ -1,10 +1,38 @@
 #include "blog.h"
 
+#define HTTP_200_HEADER "HTTP/1.1 200 OK"
+#define HTTP_200_HEADER_LENGTH 15
+
+// HTTP headers
+char *add_200_header(char *body) {
+    char *header = (char *) malloc((HTTP_200_HEADER_LENGTH + 1) * sizeof(char));
+    strcpy(header, HTTP_200_HEADER);
+
+    // Append body to header
+    size_t body_len = strlen(body);
+    char *output = (char *) malloc((HTTP_200_HEADER_LENGTH + body_len + 1) * sizeof(char));
+    strcpy(output, header);
+    strcat(output, body);
+    
+    // Free memory
+    free(header);
+    return output;
+}
+
 int main(int argc, char **argv) {
+    // Five messages to prevent caching
+    char *messages[] = {
+        "\n\n<html><body><p>abcdef ghij klmn</p></body></html>",
+        "\n\n<html><body><p>normal text here</p></body></html>",
+        "\n\n<html><body><p>helpp im getting</p></body></html>",
+        "\n\n<html><body><p>123456 7890123 4</p></body></html>",
+        "\n\n<html><body><p>lorem ipsum dolo</p></body></html>"
+    };
+
     int status;
     struct addrinfo hints;
     struct addrinfo *my_info;
-
+    
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
@@ -22,9 +50,8 @@ int main(int argc, char **argv) {
     // Listen for connections
     listen(sock_fd, BACKLOG);
 
-
     // Accept connections, fork, print, close
-    for (int i = 0; i < 9999; i++) {
+    for (unsigned int i = 0; i >= 0; i++) {
         // Serve one connection
         struct sockaddr_storage incoming_addr;
         socklen_t sin_size = sizeof incoming_addr;
@@ -40,17 +67,8 @@ int main(int argc, char **argv) {
             // Child process
             close(sock_fd);
 
-            // Five messages to prevent caching
-            char *messages[] = {
-                "http/1.1 200 ok\n\n<html><body><p>abcdef ghij klmn</p></body></html>",
-                "http/1.1 200 ok\n\n<html><body><p>normal text here</p></body></html>",
-                "http/1.1 200 ok\n\n<html><body><p>helpp im getting</p></body></html>",
-                "http/1.1 200 ok\n\n<html><body><p>123456 7890123 4</p></body></html>",
-                "http/1.1 200 ok\n\n<html><body><p>lorem ipsum dolo</p></body></html>"
-            };
-
-            int html_len = strlen(messages[0]);
-            int bytes_sent = send(new_fd, messages[i % 5], html_len, 0);
+            char *message = add_200_header(messages[i % 5]);
+            int bytes_sent = send(new_fd, message, strlen(message), 0);
 
             if (bytes_sent <= 0) {
                 perror("send");
@@ -60,6 +78,7 @@ int main(int argc, char **argv) {
             }
 
             close(new_fd);
+            exit(0);
         }
         
         close(new_fd);
